b9cf66202d4528c8d11bc99b990c015259f8e80c
Jon Ursenbach <jon@ursenba.ch>
2020-11-19 00:08:50 -0800

    refactor: slimming down the size of our unicode bump by removing unneeded data

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,3 +8,8 @@ test('should return true for an emoji that is supported', () => {
 test('should return false for an emoji that is supported', () => {
   expect(emojis.is('waffles')).toBeFalsy();
 });
+
+test('unicode dump should be valid json and contain emojis', () => {
+  const unicode = require('../unicode/index.json');
+  expect(unicode.length).toBeGreaterThanOrEqual(1000);
+});
