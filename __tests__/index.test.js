9b3258b1257714f0035685898195c3f84583badb
Jon Ursenbach <jon@ursenba.ch>
2021-09-16 18:19:52 -0700

    chore: setting up our code standards

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,17 @@
 const Emoji = require('../src').emoji;
+
 const emojis = new Emoji();
 
 test('should return true for an emoji that is supported', () => {
-  expect(emojis.is('dog')).toBeTruthy();
+  expect(emojis.is('dog')).toBe(true);
 });
 
 test('should return false for an emoji that is supported', () => {
-  expect(emojis.is('waffles')).toBeFalsy();
+  expect(emojis.is('waffles')).toBe(false);
 });
 
 test('unicode dump should be valid json and contain emojis', () => {
+  // eslint-disable-next-line global-require
   const unicode = require('../unicode/index.json');
   expect(unicode.length).toBeGreaterThanOrEqual(1000);
 });
--- add.js
+++ add.js
@@ -4,17 +4,19 @@ const path = require('path');
 const prog = path.basename(process.argv[1]);
 const args = process.argv.slice(2);
 if (args.length < 2) {
-  console.log(`USAGE: ./${prog} <image> <emoji name>\n`)
+  console.log(`USAGE: ./${prog} <image> <emoji name>\n`);
   process.exit(-1);
 }
 
 const image = args.shift();
 const emojiName = args.shift();
 
-sharp(image).resize(null, 40).toFile(`./src/img/${emojiName}.png`, (err, info) => {
-  if (err) {
-    throw err;
-  }
+sharp(image)
+  .resize(null, 40)
+  .toFile(`./src/img/${emojiName}.png`, err => {
+    if (err) {
+      throw err;
+    }
 
-  console.log('🦉 Emoji resized and added!');
-});
+    console.log('🦉 Emoji resized and added!');
+  });
