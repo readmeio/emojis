bea5f2312c790ade4050fc8720a587fcf3a3495b
Jon Ursenbach <jon@ursenba.ch>
2024-04-15 16:55:36 -0700

    feat: dropping support for < Node 18 + moving tests to vitest

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,21 @@
-const { expect } = require('chai');
+import { describe, it, expect } from 'vitest';
 
-const Emoji = require('../src').emoji;
+import { emoji as Emoji } from '../src';
 
 const emojis = new Emoji();
 
 describe('emojis', function () {
   it('should return true for an emoji that is supported', function () {
-    expect(emojis.is('dog')).to.be.true;
+    expect(emojis.is('dog')).toBe(true);
   });
 
   it('should return false for an emoji that is supported', function () {
-    expect(emojis.is('waffles')).to.be.false;
+    expect(emojis.is('waffles')).toBe(false);
   });
 
   it('unicode dump should be valid json and contain emojis', function () {
     // eslint-disable-next-line global-require
     const unicode = require('../unicode/index.json');
-    expect(unicode.length).to.be.at.least(1000);
+    expect(unicode.length).toBeGreaterThanOrEqual(1000);
   });
 });
